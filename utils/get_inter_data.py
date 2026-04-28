b07da024a7e79b286d18c731a17560fcf9c4a72b
Yiding Wang <76139257+Yidingwyd@users.noreply.github.com>
2026-04-28 20:30:47 +0800

    Add files via upload

--- utils/get_inter_data.py
+++ utils/get_inter_data.py
@@ -12,13 +12,42 @@ from itertools import combinations
 
 
 
-
+# For EAF dataset
 
 num = 100
-elem_dict = {'W':np.linspace(5, 11, num, endpoint=True),
-             'Mo':np.linspace(0, 3, num, endpoint=True),
-             'Zr':np.linspace(0, 2, num, endpoint=True)}
+elem_dict = {'Al':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True),
+             'Cr':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True),
+             'Mo':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True)}
 
+# For HEAC dataset
+
+# elem_dict = {'one':[26,27,28,29,30],
+#              'two':[26,27,28,29,30],
+#              'three':[26,27,28,29,30],
+#              'four':[26,27,28,29,30],
+#              'five':[26,27,28,29,30],
+#              'six':[26,27,28,29,30],
+#              'seven':[26,27,28,29,30],
+#              'eight':[26,27,28,29,30],
+#              'nine':[26,27,28,29,30],
+#              'ten':[26,27,28,29,30],
+#              'eleven':[26,27,28,29,30],
+#              'twelve':[26,27,28,29,30],
+#              'thirteen':[26,27,28,29,30],
+#              'fourteen':[26,27,28,29,30],
+#              'fifteen':[26,27,28,29,30],
+#              'sixteen':[26,27,28,29,30]}
+
+# For Nb dataset
+
+# num = 100
+# elem_dict = {'W':np.linspace(5, 11, num, endpoint=True),
+#              'Mo':np.linspace(0, 3, num, endpoint=True),
+#              'Zr':np.linspace(0.05, 2, num, endpoint=True),
+#              'C':np.linspace(0, 0.2, num, endpoint=True),
+#              'Hf':np.linspace(0, 7, num, endpoint=True),
+#              'Ta':np.linspace(0, 10, num, endpoint=True),
+#              'N':np.linspace(0, 0.3, num, endpoint=True)}
 
 
 for name1, name2 in combinations(elem_dict.keys(), 2):
@@ -44,8 +73,8 @@ for name1, name2 in combinations(elem_dict.keys(), 2):
     df = pd.DataFrame({'material_id':id_list,
                        'composition':comp_list,
                        'target':target_list})
-    df.to_csv('./1/0-0/'+ name1+ name2 +'.csv', index=False)
-    print("python predict.py --sample ./Kfold/WJPXY_1450_new/1/0-0/"+name1+name2+".csv --embedding ./Kfold/WJPXY_1450_new/Nb_ebd.json --modelpath ./Kfold/WJPXY_1450_new/0-0best.pth.tar --savepath ./Kfold/WJPXY_1450_new/1/0-0/"+name1+name2+".xlsx")
+    df.to_csv('./0-0/'+ name1+ name2 +'.csv', index=False)
+    print("python predict.py --sample ./example/0-0/"+name1+name2+".csv --embedding ./example/onehot-embedding.json --modelpath ./example/0-0/best.pth.tar --savepath ./example/0-0/"+name1+name2+".xlsx")
 
 
 
--- utils/get_main_data.py
+++ utils/get_main_data.py
@@ -9,12 +9,41 @@ import pandas as pd
 import numpy as np
 
 
-
+# For EAF dataset
 num = 100
-elem_dict = {'W':np.linspace(5, 11, num, endpoint=True),
-             'Mo':np.linspace(0, 3, num, endpoint=True),
-             'Zr':np.linspace(0, 2, num, endpoint=True)}
+elem_dict = {'Al':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True),
+             'Cr':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True),
+             'Mo':np.linspace(-np.pi/2, np.pi/2, num, endpoint=True)}
 
+# For HEAC dataset
+
+# elem_dict = {'one':[26,27,28,29,30],
+#              'two':[26,27,28,29,30],
+#              'three':[26,27,28,29,30],
+#              'four':[26,27,28,29,30],
+#              'five':[26,27,28,29,30],
+#              'six':[26,27,28,29,30],
+#              'seven':[26,27,28,29,30],
+#              'eight':[26,27,28,29,30],
+#              'nine':[26,27,28,29,30],
+#              'ten':[26,27,28,29,30],
+#              'eleven':[26,27,28,29,30],
+#              'twelve':[26,27,28,29,30],
+#              'thirteen':[26,27,28,29,30],
+#              'fourteen':[26,27,28,29,30],
+#              'fifteen':[26,27,28,29,30],
+#              'sixteen':[26,27,28,29,30]}
+
+# For Nb dataset
+
+# num = 100
+# elem_dict = {'W':np.linspace(5, 11, num, endpoint=True),
+#              'Mo':np.linspace(0, 3, num, endpoint=True),
+#              'Zr':np.linspace(0.05, 2, num, endpoint=True),
+#              'C':np.linspace(0, 0.2, num, endpoint=True),
+#              'Hf':np.linspace(0, 7, num, endpoint=True),
+#              'Ta':np.linspace(0, 10, num, endpoint=True),
+#              'N':np.linspace(0, 0.3, num, endpoint=True)}
 
 comp_list, target_list = [], []
 
